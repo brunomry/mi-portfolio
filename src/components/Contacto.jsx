4265ea7b3b4c5c1e6f8cea1775a6566c1a40ed4a
brunomry <brunomadozzo@gmail.com>
2024-06-26 03:05:37 -0300

    Agregue seccion de contacto en componente Contacto y FormContacto con titulo, descripcion y formulario respectivamente

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import FormContacto from './Contacto/FormContacto';
 
 const Contacto = () => {
     return (
-        <div>
-            
-        </div>
+        <article className='containerContact py-4 py-md-5 d-flex flex-column gap-2 gap-sm-4 justify-content-center align-items-center px-3 section'>
+            <h2 className='display-6'>Contáctame</h2>
+            <p className='descriptionContact'>Para colaborar o contratar mis servicios, no dudes en ponerte en contacto conmigo a través de mis redes sociales o completando el formulario a continuación. </p>
+            <FormContacto></FormContacto>
+        </article>
     );
 };
 
--- src/components/Contacto/FormContacto.jsx
+++ src/components/Contacto/FormContacto.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import {Button, Form} from 'react-bootstrap';
+
 
 const FormContacto = () => {
     return (
-        <div>
-            
+        <Form className='border-secondary px-2 px-sm-5 pt-md-4 pb-3 rounded-2 formContact'>
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor='name'>Nombre y Apellido:</Form.Label>
+        <Form.Control id='name' className='section input border-secondary' type="text" placeholder="Ingresa tu nombre completo" required/>
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor='email'>Email:</Form.Label>
+        <Form.Control id='email' className='section input border-secondary' type="email" placeholder="Ingresa tu correo" required/>
+      </Form.Group>
+      <Form.Group className="mb-3">
+      <Form.Label htmlFor='message'>Mensaje:</Form.Label>
+      <Form.Control as="textarea" className='section input border-secondary textarea' id='message' type="text" placeholder="Ingresa tu mensaje" required/>
+      </Form.Group>
+      <div className="d-flex justify-content-sm-end">
+      <button type="submit" className='btn btnSocial text-white'>
+        Enviar
+      </button>
         </div>
+   
+    </Form>
     );
 };
 
