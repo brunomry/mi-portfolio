be6bfd2512df2d485b6a2dd747fb3041010e82c3
brunomry <brunomadozzo@gmail.com>
2024-07-04 14:45:06 -0300

    Agregue ruta a linkedin en footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer className='w-100 py-3'>
-            <p className='text-center text-dark'>&copy;2024. Desarrollado por <a className='fw-bold text-decoration-none' href='' target='_blank'>Bruno Madozzo</a>.</p>
+            <p className='text-center text-dark'>&copy;2024. Desarrollado por <a className='fw-bold text-decoration-none' href="https://www.linkedin.com/in/bruno-madozzo/" target='_blank'>Bruno Madozzo</a>.</p>
         </footer>
     );
 };
