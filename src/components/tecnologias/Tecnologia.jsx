a9c60b6418cd9214b476eb1d8c4a865cb7df813a
brunomry <brunomadozzo@gmail.com>
2024-07-05 03:03:55 -0300

    agregue animacion a imagenes de tecnologias y hover a botones de proyectos

--- src/components/tecnologias/Tecnologia.jsx
+++ src/components/tecnologias/Tecnologia.jsx
@@ -1,6 +1,6 @@
 const Tecnologia = ({ tech }) => {
   return (
-    <figure key={tech.name} className="text-center text-white tech p-5">
+    <figure key={tech.name} className="text-center text-white tech p-5 shadow">
       <img src={tech.icon} alt={tech.name} className="img-fluid iconTech" title={tech.name}/>
       <figcaption className="mt-1 text-dark fw-bold">{tech.name}</figcaption>
     </figure>
