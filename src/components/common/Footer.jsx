0ed2da7408e0936c7d890130c6e9d6cfef6fcee1
Bruno Madozzo <brunomadozzo@gmail.com>
2026-01-12 01:49:50 -0300

    actualice sitemap

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="block w-[100%] py-8 md:py-[50px] bg-[#fff] px-[8px]">
       <p className="text-center text-[#a1a1a1] flex flex-col md:flex-row justify-center gap-1 font-light text-[14px]">
         <a
-          className="  lg:hover:underline"
+          className="lg:hover:underline"
           href="https://www.linkedin.com/in/bruno-madozzo/"
           target="_blank"
           title="Visita mi perfil de LinkedIn"
