ecd1290356088c040b7ed1ac169edf1b52ec94f2
Bruno Madozzo <brunomadozzo@gmail.com>
2026-01-16 13:04:55 -0300

    modifique color de titulo en seccion tecnologias

--- src/components/technologies-section/TechnologiesSection.jsx
+++ src/components/technologies-section/TechnologiesSection.jsx
@@ -7,7 +7,7 @@ const TechnologiesSection = () => {
       className="w-[100%] py-12 xl:py-20 2xl:py-40 px-4 md:px-8 lg:px-[100px] xl:px-[120px] 2xl:px-[150px] bg-[#FAFAFA] text-[#1d2a4d] flex flex-col gap-4 md:gap-6 xl:gap-12"
       id="tecnologías"
     >
-      <h2 className="font-black text-[24px] md:text-[28px] xl:text-[32px] 2xl:text-[36px] text-[#000]">
+      <h2 className="font-black text-[24px] md:text-[28px] xl:text-[32px] 2xl:text-[36px] text-[#222]">
         Tecnologías
       </h2>
       <p className="text-[#555] text-sm md:text-base font-light">
