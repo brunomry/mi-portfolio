ea0aed35299aba73ec154e78d348dc87d9e58d96
Bruno Madozzo <brunomadozzo@gmail.com>
2026-01-16 12:42:45 -0300

    corregi estilos de descripcion en service card

--- src/components/services-section/ServiceCard.jsx
+++ src/components/services-section/ServiceCard.jsx
@@ -20,7 +20,7 @@ const ServiceCard = ({ service }) => {
             {service.service}
           </h5>
         </div>
-        <p className="text-[#5text-sm55]  font-light leading-relaxed">
+        <p className="text-[#555] text-sm md:text-base font-light leading-relaxed">
           {service.description}
         </p>
         {service.tags && (
